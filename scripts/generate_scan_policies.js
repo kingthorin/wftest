d74b1513a23ce19f1cf81ae759ac3e6359746e5b
Rick M <kingthorin@users.noreply.github.com>
2024-11-27 10:57:13 -0500

    Update generate_scan_policies.js

--- scripts/generate_scan_policies.js
+++ scripts/generate_scan_policies.js
@@ -3,15 +3,14 @@
 // The policies are created after starting a ZAP weekly release with the '-addoninstallall' option.
 
 // Change the DIR below to match the local directory containing the alert files
-var LIST_FILE = "/zap/wrk/list.txt";
+var LIST_FILE = "/zap/wrk/API.policy";
 
 var FileWriter = Java.type('java.io.FileWriter');
 var PrintWriter = Java.type('java.io.PrintWriter');
 var PolicyTag = Java.type ('org.zaproxy.addon.commonlib.PolicyTag')
 var extAscan = control.getExtensionLoader().getExtension(org.zaproxy.zap.extension.ascan.ExtensionActiveScan.NAME);
 
-var whatPolicy = "POLICY_SEQUENCE";
-var includeNames = false;
+var whatPolicy = "POLICY_API";
 
 var plugins = extAscan
   .getPolicyManager()
@@ -22,19 +21,31 @@ var plugins = extAscan
   .sort(function(a, b){return a.getId() - b.getId()});
 
 // Dump out the plugin IDs
-var fw = new FileWriter(LIST_FILE);
+var fw = new FileWriter(POLICY_FILE);
 var pw = new PrintWriter(fw);
+
+pw.println("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>");
+pw.println("<configuration>");
+pw.println("    <policy>Sequence</policy>");
+pw.println("    <scanner>">);
+pw.println("        <level>OFF</level>");
+pw.println("        <strength>MEDIUM</strength>");
+pw.println("    </scanner>");
+pw.println("    <plugins>");
+
 for (var i = 0; i < plugins.length; i++) {
   try {
     if (plugins[i].getAlertTags() != null && plugins[i].getAlertTags().keySet().contains(whatPolicy)) {
-      if (includeNames) {
-        pw.println(plugins[i].getId() + "\t" + plugins[i].getName());
-      } else {
-        pw.println(plugins[i].getId());
-      }
+      pw.println("            <p" + plugins[i].getId() + ">");
+      pw.println("                <name>" + plugins[i].getName() + "</name>");
+      pw.println("                <enabled>true</enabled>");
+      pw.println("                <level>MEDIUM</level>");
+      pw.println("            <p" + plugins[i].getId() + ">");
     }
   } catch (e) {
     print(e);
   }
 }
+pw.println("    </plugins>");
+pw.println("<configuration>");
 pw.close();
